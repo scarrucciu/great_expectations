5650b38098e0771da36f691e9b9d4d304ce6404c
Abe <abegong@gmail.com>
2019-06-09 00:39:44 -0700

    Better output

--- great_expectations/cli/profile.py
+++ great_expectations/cli/profile.py
@@ -9,15 +9,19 @@ def profile_everything(context):
 
     datasource_name = 'data__local_dir'
 
-    cli_message("Profiling")
     ds = context.get_datasource(datasource_name)
     named_data_assets = list(
         ds.list_available_data_asset_names()[0]["available_data_asset_names"]
     )
-    print(named_data_assets, len(named_data_assets))
+    cli_message("Profiling %d data assets..." % len(named_data_assets))
+    # print(named_data_assets, len(named_data_assets))
 
     for asset in named_data_assets:
-        cli_message("\t")
         batch = context.get_batch("data__local_dir", asset)
         batch.autoinspect(ge.dataset.autoinspect.pseudo_pandas_profiling)
+        expectations = batch.get_expectations(suppress_warnings=True)
         batch.save_expectations(suppress_warnings=True)
+        cli_message("\t%d expectations created in %s" % (
+            len(expectations["expectations"]),
+            asset,
+        ))
