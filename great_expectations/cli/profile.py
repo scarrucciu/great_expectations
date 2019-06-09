72da141260800337fc8725f7e0bbfebbf52c20b6
Abe <abegong@gmail.com>
2019-06-09 00:33:40 -0700

    Implement profile_everything, badly

--- great_expectations/cli/profile.py
+++ great_expectations/cli/profile.py
@@ -1,7 +1,23 @@
 import click
 from .supporting_methods import cli_message
 
+import great_expectations as ge
+
 
 def profile_everything(context):
+    #!!! FIXME: This is hardcoded for now.
 
-    pass
+    datasource_name = 'data__local_dir'
+
+    cli_message("Profiling")
+    ds = context.get_datasource(datasource_name)
+    named_data_assets = list(
+        ds.list_available_data_asset_names()[0]["available_data_asset_names"]
+    )
+    print(named_data_assets, len(named_data_assets))
+
+    for asset in named_data_assets:
+        cli_message("\t")
+        batch = context.get_batch("data__local_dir", asset)
+        batch.autoinspect(ge.dataset.autoinspect.pseudo_pandas_profiling)
+        batch.save_expectations(suppress_warnings=True)
--- great_expectations/cli/render.py
+++ great_expectations/cli/render.py
@@ -3,4 +3,15 @@ from .supporting_methods import cli_message
 
 
 def render_everything(context):
-    pass
+    ds = context.get_datasource('data__local_dir')
+    named_data_assets = list(
+        ds.list_available_data_asset_names()[0]["available_data_asset_names"]
+    )
+
+    for asset in named_data_assets:
+        print(asset)
+        batch = context.get_batch("data__local_dir", asset)
+        expectations = context.get_expectations(
+            "notable_works_by_charles_dickens_MODIFIED")
+    #     batch.autoinspect(ge.dataset.autoinspect.pseudo_pandas_profiling)
+    #     batch.save_expectations()
